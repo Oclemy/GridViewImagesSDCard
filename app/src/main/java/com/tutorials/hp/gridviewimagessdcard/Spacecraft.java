8e104196315397aa2b65f074322e6fb3be2021d3
Oclemy <oclemmi@gmail.com>
2016-11-19 21:52:39 -0800

    Initial Commit

---
+++ app/src/main/java/com/tutorials/hp/gridviewimagessdcard/Spacecraft.java
@@ -0,0 +1,28 @@
+package com.tutorials.hp.gridviewimagessdcard;
+
+import android.net.Uri;
+
+/**
+ * Created by Oclemy on 7/28/2016 for ProgrammingWizards Channel and http://www.camposha.com.
+ */
+public class Spacecraft {
+
+    private String name;
+    private Uri uri;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Uri getUri() {
+        return uri;
+    }
+
+    public void setUri(Uri uri) {
+        this.uri = uri;
+    }
+}
